c38fb3b5ad0da1fac23c93a63e65759d7a367804
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 06:49:01 -0800

    mandatory

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+ /**
+  * main -Return lower-case and upper-case alphabet
+  * ch: variable alphabet
+  *
+  * Return: correctly
+  *
+  */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	 for (ch = 'A'; ch <= 'Z'; ch++)
+        {
+                putchar(ch);
+        }
+	putchar('\n');
+	return (0);
+}
