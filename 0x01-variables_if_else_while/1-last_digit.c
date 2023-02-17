c9d5c37485fe2df91e488e22040f0ca36ee8f2b5
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 04:49:14 -0800

    mandatory

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,13 +17,13 @@ int main(void)
 	digit = n % 10;
 	if (digit > 5)
 	{
-	       printf("Last Digit of %d is %d and is greater than 5\n", n, digit);
-	} else if (digit == 0) 
+	       printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	} else if (digit == 0)
 	{
-		printf("Last Digit of %d is %d and is 0\n", n, digit);
-	} else 
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	} else
 	{
-		printf("Last Digit of %d is %d and is less than 6 and not 0\n", n, digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
 	return (0);
 }
