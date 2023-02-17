1014cc9e2292d14d90a0b2fed9f8e3662b9eff42
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 04:22:19 -0800

    mandatory

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main -print whether the number stored in the variable n is positive or negative.
+ * main -print whether variable n is positive or negative.
  * n -random number
  *
  */
