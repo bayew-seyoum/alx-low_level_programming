c75ad89e511224b7bb26c010adec8ea08d3749ee
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 09:32:39 -0800

    mandatory

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,12 @@ int main(void)
 	int x;
 	char y;
 
-	for (x = 0; x <16; x++)
+	for (x = 0; x < 10; x++)
 		putchar('0'+ x);
 
-	for (y = 'a'; y <= 'f'; y++)
+	for (y = 'a'; y < 'g'; y++)
 		putchar(y);
+
 	putchar('\n');
 	return (0);
 }
