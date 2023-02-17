ad43ac63720627e6e4a26df4e7dc40cc5c26a2d8
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 08:05:51 -0800

    mandatory

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit number
+ * x: variable number
+ * print single digit number of base ten starting from zero
+ *
+ * Return: alwayse reuren zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar("$x");
+	putchar('\n');
+	return (0);
+}
