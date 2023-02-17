10bb438352330dea76108f6216a07887ed4cfdd3
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 13:02:47 -0800

    mandatory

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
- /**
-  * main -Return lower-case alphabet
-  * ch: variable alphabet
-  * program that prints the alphabet in lowercase,
-  * followed by a new line.
-  * 
-  * Return: correctly
-  */
+/**
+ * main -Return lower-case alphabet
+ * ch: variable alphabet
+ * program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: correctly
+ */
 int main(void)
 {
 	char ch;
