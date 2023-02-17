5950cb0f6a3f51dd928ad6a5a40dfbf6660a906e
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 08:40:50 -0800

    mandatory

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints lower-case alphabet in reverse
+ * ch: variable character
+ * program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ *
+ * Return: always correct or zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	       putchar(ch);
+	putchar('\n')
+	return (0)
+}	
+
