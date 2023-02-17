b677453493e49b5701c6c4426d9283421fc09884
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 09:08:54 -0800

    mandatory

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h)
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * x: variable number
+ * program that prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ *
+ * Return:zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <16; x++)
+		putchar('0'+ x)
+	char y;
+
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y)
+	putchar('\n')
+	return (0)
+}
