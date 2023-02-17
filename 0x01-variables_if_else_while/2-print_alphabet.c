87a9a346e97a76cd8f99800ce52f51562e806e70
bayew-seyoum <sytmbm333@gmail.com>
2023-02-17 05:14:44 -0800

    mandatory

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,15 @@
   * ch: variable alphabet
   *
   * Reuren: correctly
-  * */
-int main()
+  */
+int main(void)
 {
-    char ch = 'a';
-    while(ch <= 'z')  
-    {
-	putchar(ch);	
-        ch++;
-    }
-    putchar('\n');
-    return (0);
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
