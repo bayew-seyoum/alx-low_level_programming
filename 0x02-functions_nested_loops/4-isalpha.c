c0bd49ba8f739e6da702cc3abb20d7ed8547b6fc
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 13:44:45 -0800

    mandatory

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
   * _isalpha -a function that checks for alphabetic character.
+  *
   * ch: variable character to be checked
   *
   * Return: 1 for alphabetic character or 0 for anything else
