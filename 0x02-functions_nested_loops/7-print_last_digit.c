b3f8a1f0b701392d7bcb4b2ae95df50601617bb0
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 14:05:09 -0800

    mandatory

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include "6-abs.c"
+/**
+ * print_last_digit -a function that prints the last digit of a number.
+ *
+ * x: number
+ * Return: last digit of x
+ *
+ */
+
+int print_last_digit(int x)
+{
+	_putchar('0' + _abs(x % 10));
+	return (_abs(x % 10));
+}
