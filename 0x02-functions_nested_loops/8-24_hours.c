1652459abc577efa39e45a9a3c17345d9d3e39b1
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 14:21:36 -0800

    mandatory

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+  * jack_bauer - Prints every minute of the day of Jack Bauer
+  * i,j,k,l: variable
+  * Return: ...
+  */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
+			{
+				for (k = 0; k <= 5; k++)
+				{
+					for (l = 0; l <= 9; l++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
