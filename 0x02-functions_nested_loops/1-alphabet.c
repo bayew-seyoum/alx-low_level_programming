3d43cd6ebe3e6e74dc5c6139f9b7ca231fbcbf23
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 11:40:26 -0800

    mandatory

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ * ch: variable
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
