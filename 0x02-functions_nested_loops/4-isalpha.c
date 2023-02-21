9f05dbb52a2c7ab46aca00c8c7d5ef1f2e55c628
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 13:36:16 -0800

    mandatory

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _isalpha -a function that checks for alphabetic character.
+  * ch: variable (character to be checked)
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
+int _isalpha(int ch)
+{
+	if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
+}
