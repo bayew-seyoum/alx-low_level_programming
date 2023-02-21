bdb02cc80a5070ce91886df984c132bbedf07d0b
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 15:21:52 -0800

    mandatory

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ * i,x,y,z: variable
+ * Return: ...
+ */
+int main(void)
+{
+	int i, x = 1, y = 2, z;
+
+	for (i = 1; i <= 98; i++)
+	{
+		z = x + y
+		if (i != 98)
+		{
+			printf("%lu, ", z)
+		} else
+		{
+			printf("%lu\n", z)
+		}
+		x = y
+		y = z
+	}
+	return (0)
+}
