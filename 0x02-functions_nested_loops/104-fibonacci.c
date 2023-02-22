ad8bbd86aee6096c2bb974429aa11c99daac2085
bayew-seyoum <sytmbm333@gmail.com>
2023-02-21 22:32:11 -0800

    advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,17 @@
 int main(void)
 {
 	int i;
-	long x = 0, y = 1, z;
+	long long x = 0, y = 1, z
 
 	for (i = 1; i <= 98; i++)
 	{
 		z = x + y;
 		if (i != 98)
 		{
-			printf("%ld, ", z);
+			printf("%lld, ", z);
 		} else
 		{
-			printf("%ld\n", z);
+			printf("%lld\n", z);
 		}
 		x = y;
 		y = z;
