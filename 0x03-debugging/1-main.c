49dcb907f5a08d04936d134911cd1bf490c9e406
bayew-seyoum <sytmbm333@gmail.com>
2023-02-22 22:42:46 -0800

    mandatory

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
 
 	for (; i < 10; i++)
@@ -18,6 +18,6 @@ int main(void)
 		putchar(i);
 	}
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
