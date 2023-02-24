0395fe5375949e046f205adf2cd323945b29dda1
bayew-seyoum <sytmbm333@gmail.com>
2023-02-24 14:39:50 -0800

    advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,16 +17,41 @@ int main(void)
 	printf(", ");
 	printf("%lu", j);
 	printf(", ");
-	while (counter < 98)
+	while (counter < 93)
 	{
 		counter++;
 		printf("%lu", k);
 		i = j;
 		j = k;
 		k = i + j;
-		if (counter < 98)
+		if (counter < 93)
 		{
 			printf(", ");
+		}
+	}
+
+	i1 = i / 10000000000;
+	i2 = i % 10000000000;
+	j1 = j / 10000000000;
+	j2 = j % 10000000000;
+	while (counter >= 93 && counter < 98)
+        {
+		counter++;
+		k1 = i1 + j1;
+		k2 = i2 + j2;
+		if (k2 > 9999999999)
+		{
+			k1 = k1 + 1;
+			k2 = k2 % 10000000000;
+		}
+		printf("%lu%lu", k1, k2);
+		i1 = j1;
+		i2 = j2;
+		j1 = k1;
+		j2 = k2;
+		if (counter < 98)
+		{
+			printf(", ")
 		}
 	}
 	printf("\n");
