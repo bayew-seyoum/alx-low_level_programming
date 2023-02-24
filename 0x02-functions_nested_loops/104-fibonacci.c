c8eb7e83a4f1031aee8080030dda332a57da77f0
bayew-seyoum <sytmbm333@gmail.com>
2023-02-24 15:23:53 -0800

    advanced

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int counter = 2;
-
 	unsigned long i = 1, i1, i2;
 	unsigned long j = i + 1, j1, j2;
 	unsigned long k = i + j, k1, k2;
@@ -17,42 +16,32 @@ int main(void)
 	printf(", ");
 	printf("%lu", j);
 	printf(", ");
-	while (counter < 922)
+	while (counter < 92)
 	{
 		counter++;
 		printf("%lu", k);
-		i = j;
-		j = k;
-		k = i + j;
+		i = j, j = k, k = i + j;
 		if (counter < 98)
 		{
 			printf(", ");
 		}
 	}
-
-	i1 = i / 10000000000;
-	i2 = i % 10000000000;
-	j1 = j / 10000000000;
-	j2 = j % 10000000000;
+	i1 = i / 10000000000, i2 = i % 10000000000;
+	j1 = j / 10000000000, j2 = j % 10000000000;
 	while (counter >= 92 && counter < 98)
-        {
+	{
 		counter++;
-		k1 = i1 + j1;
-		k2 = i2 + j2;
+		k1 = i1 + j1, k2 = i2 + j2;
 		if (k2 > 9999999999)
 		{
-			k1 = k1 + 1;
-			k2 = k2 % 10000000000;
+			k1 = k1 + 1, k2 = k2 % 10000000000;
 		}
 		printf("%lu%lu", k1, k2);
-		i1 = j1;
-		i2 = j2;
-		j1 = k1;
-		j2 = k2;
 		if (counter < 98)
 		{
 			printf(", ");
 		}
+		i1 = j1, i2 = j2, j1 = k1, j2 = k2;
 	}
 	printf("\n");
 	return (0);
