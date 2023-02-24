e0493aa27db4c2c1ef8178baf115cfcabba73fbd
bayew-seyoum <sytmbm333@gmail.com>
2023-02-24 12:35:33 -0800

    advanced

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,20 +3,21 @@
 
 /**
   * main - Prints the largest prime factor of the number 612852475143
-  *
+  * i: variable
+  * maxf: maximum prime factor
   * Return: Always 0 (Success)
   */
 int main(void)
 {
 	long i, maxf;
 	long n = 612852475143;
-	double square = sqrt(number);
+	double square = sqrt(n);
 
 	for (i = 1; i <= square; i++)
 	{
 		if (n % i == 0)
 		{
-			maxf = n /  x;
+			maxf = n /  i;
 		}
 	}
 
