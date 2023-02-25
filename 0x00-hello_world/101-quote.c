c9e764d8fffbcd179e086393dbd7fccf04535a48
bayew-seyoum <sytmbm333@gmail.com>
2023-02-24 16:01:22 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /*
- * main: - prints and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main -prints and that piece of art is useful
  * followed by a new line, to standard error.
  * @2: line of code
  * @and that piece of art is useful\" - Dora Korpar, 2015-10-19\n: printed
