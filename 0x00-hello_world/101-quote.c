0c856401363d7cb51a472e9ac28a0b47e5a13bd5
bayew-seyoum <sytmbm333@gmail.com>
2023-02-24 16:04:14 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 /*
  * main -prints and that piece of art is useful
- * followed by a new line, to standard error.
+ * Description: followed by a new line, to standard error.
  * @2: line of code
  * @and that piece of art is useful\" - Dora Korpar, 2015-10-19\n: printed
  * @59: size of data
