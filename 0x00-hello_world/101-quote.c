79ae6c1d2b517300f1d04c16456b1b4918cce147
bayew-seyoum <sytmbm333@gmail.com>
2023-02-24 16:13:29 -0800

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
-/*
- * File: 101-quote.c
- * Auth: bayew
- */
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main -prints and that piece of art is useful
- * Description: followed by a new line, to standard error.
- * @2: line of code
- * @and that piece of art is useful\" - Dora Korpar, 2015-10-19\n: printed
- * @59: size of data
- * Return: one
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	write(2,
