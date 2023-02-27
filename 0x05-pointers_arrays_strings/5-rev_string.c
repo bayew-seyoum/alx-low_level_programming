4f8e8f0137a42da1f44eb72f088fb4774ab02cb9
bayew-seyoum <sytmbm333@gmail.com>
2023-02-27 06:01:13 -0800

    mandatory

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "2-strlen.c"
-/*
- * rev_string -a function that reverses a string.
+/**
+ * rev_stri - a function that reverses a string.
  *
  * @s: string to print
  */
