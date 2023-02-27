131e2cb8c01bda2651c6d095d13bdd441f5f49ff
bayew-seyoum <sytmbm333@gmail.com>
2023-02-27 02:22:18 -0800

    mandatory

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_strlen - returns the length of a string.
- *@s: string variable
- *
+ * _strlen - returns the length of a string.
+ * @s: string variable
+ * Return: streng length i
  */
 int _strlen(char *s)
 {
