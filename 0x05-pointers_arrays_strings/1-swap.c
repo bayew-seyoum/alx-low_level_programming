45bd56b025eb8ab037ffb1cdbac9c733d7f99a91
bayew-seyoum <sytmbm333@gmail.com>
2023-02-27 02:03:59 -0800

    mandatory

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int -swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+	int y = *b;
+	*a = x;
+	*b = y;	
+}
