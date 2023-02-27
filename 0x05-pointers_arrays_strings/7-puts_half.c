09f91caff961a34bb68bd10bae238431cdab16bc
bayew-seyoum <sytmbm333@gmail.com>
2023-02-27 06:41:01 -0800

    mandatory

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,15 @@
 void puts_half(char *str)
 {
 	int i;
-	for (i = 0; i <= (_strlen(str) / 2); i++)
-		_putchar(str[i]);
+	if (_strlen(str)/2 == 0)
+	{
+		for (i = _strlen(str) / 2; i < _strlen(str); i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (_strlen(str) - 1) / 2; i < strlen(str); i++)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
