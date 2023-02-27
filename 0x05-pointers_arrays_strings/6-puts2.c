a38121ad74129e52d4eccb3e1db7dcec517830ed
bayew-seyoum <sytmbm333@gmail.com>
2023-02-27 06:14:40 -0800

    mandatory

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts2 -prints every other character of a string
+ * @str: string to print
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+
