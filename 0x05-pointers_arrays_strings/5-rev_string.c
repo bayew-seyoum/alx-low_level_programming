0d474bd544146c3b02a8162a41ce9e2c22a0e803
bayew-seyoum <sytmbm333@gmail.com>
2023-02-27 05:55:59 -0800

    mandatory

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#in clude "main.h"
+#include "2*strlen.c"
+/*
+ * rev_string -a function that reverses a string.
+ *
+ * @s: string to print
+ */
+void rev_string(char *s)
+{
+	int i;
+	char t;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
+}
