d330d2beba790ae0fbc0a1b900a359e9da7416cb
bayew-seyoum <sytmbm333@gmail.com>
2023-03-02 11:58:18 -0800

    advanced

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,18 +5,38 @@
  */
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	if (n < 0)
 	{
 		_putchar('-');
+		n += 1;
 		n *= -1;
+		n++;
 	}
+	abs = n;
+	abSCount = n;
 
-	x = n;
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
 
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
