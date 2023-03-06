d3dbb3a8269ad715cd4a10443790f8481683f1af
bayew-seyoum <sytmbm333@gmail.com>
2023-03-06 12:23:34 -0800

    mandatory

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j, diagsum1 = 0, diagsum2 = 0;
-	
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
@@ -19,12 +19,12 @@ void print_diagsums(int *a, int size)
 	}
 
 	for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < size; j++)
-                {
-                        if (i + j == size -1)
-                                diagsum2 = diagsum2 + a[i][j];
-                }
-        }
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i + j == size - 1)
+				diagsum2 = diagsum2 + a[i][j];
+		}
+	}
 	printf("%d, %d\n", diagsum1, diagsum2);
 }
