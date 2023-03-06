1cfb3f559d48d10a0042fd59ca5a164525d00d23
bayew-seyoum <sytmbm333@gmail.com>
2023-03-06 04:19:02 -0800

    mandatory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcpy - a function that copies memory area.
  * function copies n bytes from memory area src to memory area dest
  * @dest: destination memory area
- * @scr: source memory area
+ * @src: source memory area
  * @n: the size of memory to copy
  * Return: dest
  */
