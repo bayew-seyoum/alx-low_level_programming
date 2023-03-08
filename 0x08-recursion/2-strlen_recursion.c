8813f21a72eac59046f682ff8ce596fc7e31127d
bayew-seyoum <sytmbm333@gmail.com>
2023-03-08 09:22:54 -0800

    mandatory

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: zero or integer value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
