080d9ab5378ab58acbc078b3f2685656fe37b72c
bayew-seyoum <sytmbm333@gmail.com>
2023-03-08 12:50:56 -0800

    mandatory

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /*
- * checker - checks the input number from n to the base
+ * checker - checks the input number from i to the base n
  * @i: number is squared and compared against base
  * @n: base number to check
  * Return: natural square root of number base
  */
-
 int checker(int n, int i)
 {
-        if (i * i == n)
-                return (i);
-        if (i * i > n)
-                return (-1);
-        return (checker(i + 1, n));
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (checker(n, i + 1));
 }
 /**
  * _sqrt_recursion - a function that returns the natural square root 
@@ -22,5 +21,5 @@ int checker(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-	return (checker(n, 1)); 
+	return (checker(n, 1));
 }
