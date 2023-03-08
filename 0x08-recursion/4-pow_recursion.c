7d527136cbe4e5c6bbd4160462193e32faa9207a
bayew-seyoum <sytmbm333@gmail.com>
2023-03-08 11:36:53 -0800

    mandatory

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,11 +8,11 @@
  */
 int _pow_recursion(int x, int y)
 {
-	for (y < 0)
+	if (y < 0)
 		return (-1);
-	for (y == 0)
+	if (y == 0)
 		return (1);
-	for (y == 1)
+	if (y == 1)
 		return (x);
 	return (x * _pow_recursion(x, y - 1));
 }
